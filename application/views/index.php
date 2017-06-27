7a83154542610a76bf9a6b00e2b3145f710c14d6
Artem <lanart@ex.ua>
2017-06-27 13:06:02 +0300

    commit

--- application/views/index.php
+++ application/views/index.php
@@ -1,17 +1,21 @@
 <div class="places">
-    <div class="places_in">
+    <div class="places_in" id="A">
         <?php
-        for ($i=1; $i<=4; $i++) {
-            for ($j = 1; $j <= 12; $j++) { ?>
+        for ($i=1; $i<=4; $i++) {?>
+            <span style="font-size:x-small"><?= $i;?></span>&nbsp;&nbsp;
+            <?php
+            for ($j = 1; $j <= 10; $j++) { ?>
                 <button type="button" class="btn btn-info"><?= $j ?></button>
             <?php }  ?> <br /> <?php
         }
         ?>
     </div>
-    <div class="places_in">
+    <div class="places_in" id="B">
         <?php
-        for ($i=1; $i<=4; $i++) {
-            for ($j = 1; $j <= 12; $j++) { ?>
+        for ($i=1; $i<=4; $i++) {?>
+            <span style="font-size:x-small"><?= $i;?></span>&nbsp;&nbsp;
+            <?php
+            for ($j = 1; $j <= 10; $j++) { ?>
                 <button type="button" class="btn btn-warning"><?= $j ?></button>
             <?php }  ?> <br /> <?php
         }
@@ -20,16 +24,18 @@
 
 </div>
 <div class="places">
-    <div class="places_in">
+    <div class="places_in" id="C">
         <?php
-        for ($i=1; $i<=5; $i++) {
-            for ($j = 1; $j <= 12; $j++) { ?>
+        for ($i=1; $i<=5; $i++) {?>
+            <span style="font-size:x-small"><?= $i;?></span>&nbsp;&nbsp;
+            <?php
+            for ($j = 1; $j <= 10; $j++) { ?>
                 <button type="button" class="btn btn-success"><?= $j ?></button>
             <?php }  ?> <br /> <?php
         }
         ?>
     </div>
-    <div class="places_in">
+    <div class="places_in id" id="D">
         <?php
         for ($i=1; $i<=5; $i++) {
             for ($j = 1; $j <= 12; $j++) { ?>
@@ -38,7 +44,7 @@
         }
         ?>
     </div>
-    <div class="places_in">
+    <div class="places_in" id="E">
         <?php
         for ($i=1; $i<=5; $i++) {
             for ($j = 1; $j <= 12; $j++) { ?>
@@ -49,7 +55,7 @@
     </div>
 </div>
 <div class="places">
-    <div class="places_in">
+    <div class="places_in" id="F">
         <?php
         for ($i=1; $i<=4; $i++) {
             for ($j = 1; $j <= 12; $j++) { ?>
@@ -58,7 +64,7 @@
         }
         ?>
     </div>
-    <div class="places_in">
+    <div class="places_in" id="G">
         <?php
         for ($i=1; $i<=4; $i++) {
             for ($j = 1; $j <= 12; $j++) { ?>
