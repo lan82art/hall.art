0e596fa0fa4abc422f38617e93dbd06d8c5bc40b
artem <lanart@ex.ua>
2017-06-30 16:36:30 +0300

    bjmbj

--- application/views/layout.php
+++ application/views/layout.php
@@ -45,23 +45,21 @@
     </div>
     <div class="container-fluid" style="padding: 70px 15px;">
         <div class="row">
-            <div class="col-xs-1">
-
-            </div>
-            <div class="col-xs-2" style="border: solid 1px #990099; padding: 20px 0; text-align: center;">
+            <!--<div class="col-xs-1"></div>-->
+            <div class="col-xs-1" style="border: solid 1px #990099; padding: 20px 0; text-align: center;">
                 <div><button type="button" class="btn btn-danger">100 грн.</button></div>
                 <div><button type="button" class="btn btn-info">200 грн.</button></div>
                 <div><button type="button" class="btn btn-primary">250 грн.</button></div>
                 <div><button type="button" class="btn btn-warning">300 грн.</button></div>
                 <div><button type="button" class="btn btn-success">400 грн.</button></div>
             </div>
-            <div class="col-xs-6">
+            <div class="col-xs-9">
                 <?php require_once 'application/views/'.$content;?>
             </div>
             <div class="col-xs-2">
                 <div id="order" class="order"></div>
             </div>
-            <div class="col-xs-1"></div>
+            <!--<div class="col-xs-1"></div>-->
         </div>
     </div>
 </div>
