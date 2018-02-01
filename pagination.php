56de51ce42a9115311849b3c4376f3cb2b5baaaf
Artem <lanart@ex.ua>
2018-02-01 22:17:25 +0200

    Merge branch 'master' of https://github.com/lan82art/hall.art
    # Conflicts:
    #	config.php

--- pagination.php
+++ pagination.php
@@ -1,7 +1,13 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Artem
- * Date: 01.02.2018
- * Time: 21:24
- */
+$currentPage = !empty($_GET['page']) ? $_GET['page'] : 1;
+$buttonsQnt = 6;
+$lastPage = 50;
+
+$start = ($currentPage > 4) ? ($currentPage - ceil($buttonsQnt / 2) ) : 1;
+$st = $start;
+$start = ($buttonsQnt > $lastPage - $start) ? $lastPage - $buttonsQnt : $st;
+$stop = $currentPage < ($lastPage - ceil($buttonsQnt / 2)) ? $start + $buttonsQnt : $lastPage;
+
+echo 'start '.$start.'<br/>';
+echo 'stop '.$stop.'<br/>';
+
